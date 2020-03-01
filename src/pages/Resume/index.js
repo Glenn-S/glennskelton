f504b6e855efb08f7428ed0716ac5ae5991be5be
Glenn Skelton <skeltong2@gmail.com>
2020-02-29 20:51:18 -0700

    Created Resume viewer. Created download link

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,29 +1,43 @@
 import React, { Component } from "react";
-import PDFViewer from '../../components/PDFViewer';
+//import PDFViewer from '../../components/PDFViewer';
+//import PDFJSBackend from '../../backend/pdfjs';
 //import { Document, Page, PDFDownloadLink } from "@react-pdf/renderer";
 //import App from '../../components/App';
 
 export default class Resume extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      resume: '/resume.pdf'
+    }
+  }
+
+  downloadResume = (url, name) => {
+    var link = document.createElement('a');
+    link.href = url;
+    link.click();
+  }
   
   render() {
     return (
-      <div>
+      <div style={{ height: "100%" }}>
         <h2 className="ui center aligned header">
           Resume
         </h2>
-        <p>
-          Content coming soon
-        </p>
-        <PDFViewer
-/>      </div>
+        <div className="ui placeholder segment">
+          <div className="ui icon header">
+            <i className="pdf file outline icon"></i>
+          </div>
+          <a href={this.state.resume} download className="ui primary button">Download</a>
+        </div>
+      </div>
     );
   }
 };
-
 /*
-<div style={{ width: 600 }}>
-          <PDFDownloadLink document={<App />} fileName="../../files/resume.pdf">
-            {({ blob, url, loading, }) => (loading ? 'Loading document...' : 'Download now!')}
-          </PDFDownloadLink>
-        </div>
+<PDFViewer 
+          backend={PDFJSBackend}
+          src='/resume.pdf'
+        />
 */
